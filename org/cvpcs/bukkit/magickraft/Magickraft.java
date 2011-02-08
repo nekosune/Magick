231bd7fc60fd5bd13f6e2b4030cbc42aa7e8a63d
cvpcs <root@cvpcs.org>
2011-02-07 20:23:30 -0600

    working door rune

--- org/cvpcs/bukkit/magickraft/Magickraft.java
+++ org/cvpcs/bukkit/magickraft/Magickraft.java
@@ -1,10 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.cvpcs.bukkit.magickraft;
-
 
 import org.bukkit.Server;
 import org.bukkit.event.Event.Priority;
@@ -12,27 +6,16 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.block.BlockFace;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.block.Block;
-import org.bukkit.inventory.ItemStack;
+
 import org.cvpcs.bukkit.magickraft.runes.ChronoTriggerRune;
 import org.cvpcs.bukkit.magickraft.runes.CompassRune;
+import org.cvpcs.bukkit.magickraft.runes.DoorRune;
 import org.cvpcs.bukkit.magickraft.runes.OracleRune;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Random;
 
 /**
  *
@@ -46,10 +29,6 @@ public class Magickraft extends JavaPlugin {
 	
     private RuneRunner runeRunner;
     
-    private Random rand = new Random();
-
-    private HashSet<String> tool;
-
     public Magickraft(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
         super(pluginLoader, instance, desc, folder, plugin, cLoader);
         CONFIG = new MagickraftConfig(this);
@@ -58,20 +37,16 @@ public class Magickraft extends JavaPlugin {
         RUNES.put(OracleRune.NAME, new OracleRune(this));
         RUNES.put(CompassRune.NAME, new CompassRune(this));
         RUNES.put(ChronoTriggerRune.NAME, new ChronoTriggerRune(this));
+        RUNES.put(DoorRune.NAME, new DoorRune(this));
         
         runeRunner = new RuneRunner();
     }
 
     public void onDisable() {
-        System.out.println("Magikraft unloaded!");
+        Log("unloaded!");
     }
 
     public void onEnable() {
-        registerEvents();
-        System.out.println("Magikraft " + this.getDescription().getVersion() + " loaded!");
-    }
-
-    private void registerEvents() {
     	for(Map.Entry<String, Rune> entry : RUNES.entrySet()) {
     		entry.getValue().setEnabled(CONFIG.getRuneBoolean(entry.getKey(), MagickraftConfig.RUNE_ENABLED_KEY));
     	}
@@ -79,232 +54,12 @@ public class Magickraft extends JavaPlugin {
         getServer().getPluginManager().registerEvent(Event.Type.BLOCK_RIGHTCLICKED, runeRunner, Priority.Normal, this);
         getServer().getPluginManager().registerEvent(Event.Type.BLOCK_DAMAGED, runeRunner, Priority.Normal, this);
         getServer().getPluginManager().registerEvent(Event.Type.REDSTONE_CHANGE, runeRunner, Priority.Normal, this);
+        
+        Log("loaded!");
     }
     
-    private Location getSafeLocation(Location loc)
-    {
-        boolean safe = false;
-        while(!safe)
-        {
-            safe = true;
-            if (loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).getType()!=Material.AIR)
-            {
-                safe=false;
-                loc = loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).getFace(BlockFace.UP).getLocation();
-            }
-            if (loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).getFace(BlockFace.UP).getType()!=Material.AIR)
-            {
-                safe=false;
-                loc = loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).getFace(BlockFace.UP).getLocation();
-            }
-        }
-        safe = false;
-        while(!safe)
-        {
-            safe = true;
-            if (loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).getFace(BlockFace.DOWN).getType()==Material.AIR)
-            {
-                safe = false;
-                loc = loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).getFace(BlockFace.DOWN).getLocation();
-            }
-        }
-        if ((int)loc.getX()==loc.getX())
-        {
-            loc.setX(loc.getX()+0.5);
-        }
-        if ((int)loc.getY()==loc.getY())
-        {
-            loc.setY(loc.getY()+0.5);
-        }
-        if ((int)loc.getZ()==loc.getZ())
-        {
-            loc.setZ(loc.getZ()+0.5);
-        }
-        loc.getWorld().loadChunk(loc.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).getChunk());
-        return loc;
-    }
-    public void addTool(Player player, Material material)
-    {
-        tool.add(player.getName()+"_"+material.name());
-    }
-    public void delTool(Player player, Material material)
-    {
-        tool.remove(player.getName()+"_"+material.name());
-    }
-    public boolean poweredTool(Player player, Material material)
-    {
-        return tool.contains(player.getName()+"_"+material.name());
-    }
-    public ItemStack getDrop(Block block, ItemStack itemInHand)
-    {
-        int type=block.getType().getId();
-    	int drop;
-    	int amount=1;
-        int data=0;
-        int pick = pickLevel(itemInHand.getType());
-
-    	switch (type)
-        {
-            case 1://Stone
-                if (pick>0) drop=4;
-                else drop = 0;
-                break;
-            case 2://Grass
-                drop=3;
-                break;
-            case 4://cobble
-                if (pick>0) drop=4;
-                else drop = 0;
-                break;
-            case 13://Gravel
-                if (rand.nextDouble() >= 0.9) {drop=318;}
-                else{drop=13;}
-                break;
-            case 14://gold ore
-                if (pick>0) drop=14;
-                else drop = 0;
-                break;
-            case 15://iron ore
-                if (pick>0) drop=15;
-                else drop = 0;
-                break;
-            case 16://Coal ore
-                if (pick>0) drop=263;
-                else drop = 0;
-                break;
-            case 18://Leaves
-                drop=0;
-                break;
-            case 20://Glass
-                drop=0;
-                break;
-            case 21://lapis ore
-                if (pick>0) drop=351;
-                else drop = 0;
-                data=4;
-                break;
-            case 22://lapis block
-                if (pick>0) drop=22;
-                else drop = 0;
-                break;
-            case 23://dispenser
-                if (pick>0) drop=23;
-                else drop = 0;
-                break;
-            case 41://gold block
-                if (pick>0) drop=41;
-                else drop = 0;
-                break;
-            case 42://iron
-                if (pick>0) drop=42;
-                else drop = 0;
-                break;
-            case 43://Double step
-                if (pick>0) drop=44;
-                else drop = 0;
-                break;
-            case 44://stone slab
-                if (pick>0) drop=44;
-                else drop = 0;
-                break;
-            case 47://Bookshelf
-                drop=0;
-                break;
-            case 49://obsidian
-                if (pick>0) drop=49;
-                else drop = 0;
-                break;
-            case 52://Spawner
-                drop=0;
-                break;
-            case 55://Redstone
-                drop=331;
-                break;
-            case 56://Diamond ore
-                if (pick>0) drop=264;
-                else drop = 0;
-                break;
-            case 57://diamond block
-                if (pick>0) drop=57;
-                else drop = 0;
-                break;
-            case 60://Soil
-                drop=3;
-                break;
-            case 61://furnace
-                if (pick>0) drop=61;
-                else drop = 0;
-                break;
-            case 62://Burning furnace
-                if (pick>0) drop=61;
-                else drop = 0;
-                break;
-            case 63://Sign post
-                drop=323;
-                break;
-            case 67://cobble stairs
-                if (pick>0) drop=4;
-                else drop = 0;
-                break;
-            case 68://Wall sign
-                drop=323;
-                break;
-            case 73://Redstone ore
-                if (pick>0) drop=331;
-                else drop = 0;
-                amount=4;
-                break;
-            case 74://Glowing redstone ore
-                if (pick>0) drop=331;
-                else drop = 0;
-                amount=4;
-                break;
-            case 75://Redstone torch off
-                drop=76;
-                break;
-            case 78://Snow
-                drop=0;
-                break;
-            case 79://Ice
-                drop=0;
-                break;
-            case 80://Snowblock
-                drop=332;
-                amount=4;
-                break;
-            case 82://Clay
-                drop=337;
-                amount=4;
-                break;
-            case 83://Reed
-                drop=338;
-                break;
-            case 89://Lightstone
-                drop=348;
-                break;
-            default://Items that drop their own id, like cobblestone and dirt
-                drop=type;
-    	}
-    	if(drop>0)
-        {
-            ItemStack ret = new ItemStack(drop);
-            ret.setAmount(amount);
-            if (data>0)
-            {
-                ret.setDurability((short)data);
-            }
-            return ret;
-    	}
-        return null;
-    }
-    private int pickLevel(Material mat)
-    {
-        if (mat==Material.WOOD_PICKAXE)return 1;
-        if (mat==Material.GOLD_PICKAXE)return 2;
-        if (mat==Material.STONE_PICKAXE)return 3;
-        if (mat==Material.IRON_PICKAXE)return 4;
-        if (mat==Material.DIAMOND_PICKAXE)return 5;
-        return 0;
+    public static void Log(String s) {
+    	System.out.println("[Magickraft]: " + s);
     }
 }
 
---
+++ org/cvpcs/bukkit/magickraft/runes/DoorRune.java
@@ -0,0 +1,269 @@
+package org.cvpcs.bukkit.magickraft.runes;
+
+import org.bukkit.event.block.BlockRightClickEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockDamageLevel;
+import org.bukkit.block.BlockFace;
+import org.bukkit.Material;
+import org.bukkit.Location;
+
+import org.cvpcs.bukkit.magickraft.Magickraft;
+import org.cvpcs.bukkit.magickraft.Rune;
+import org.cvpcs.bukkit.magickraft.runestruct.IRuneNode;
+import org.cvpcs.bukkit.magickraft.runestruct.RNAnything;
+import org.cvpcs.bukkit.magickraft.runestruct.RNComplex;
+import org.cvpcs.bukkit.magickraft.runestruct.RNMaterial;
+import org.cvpcs.bukkit.magickraft.runestruct.RNMaterialGroup;
+import org.cvpcs.bukkit.magickraft.runestruct.RNTier;
+import org.cvpcs.bukkit.magickraft.runestruct.RuneStructure;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+public class DoorRune extends Rune {
+	
+	public static final String NAME = "door";
+	
+    public DoorRune(Magickraft plugin) {
+        super(plugin, new RuneStructure(3, 1, 3, 2, new IRuneNode[][][]{
+        		{
+        			{
+            			RNComplex.getInstance(
+            					RNTier.getInstance(1),
+            					RNMaterialGroup.getInstance(0)),
+
+            			RNMaterial.getInstance(Material.IRON_ORE),
+
+            			RNComplex.getInstance(
+            					RNTier.getInstance(1),
+            					RNMaterialGroup.getInstance(0)),
+        			},
+        		},
+        		{
+        			{
+            			RNMaterial.getInstance(Material.IRON_ORE),
+
+            			RNAnything.getInstance(),
+
+            			RNMaterial.getInstance(Material.IRON_ORE),
+        			},
+        		},
+        		{
+        			{
+            			RNComplex.getInstance(
+            					RNTier.getInstance(1),
+            					RNMaterialGroup.getInstance(0)),
+
+            			RNComplex.getInstance(
+            					RNTier.getInstance(1),
+            					RNMaterialGroup.getInstance(0)),
+
+            			RNComplex.getInstance(
+            					RNTier.getInstance(1),
+            					RNMaterialGroup.getInstance(0)),
+        			},
+        		}
+       	}, new int[][][]{
+        		{
+        			{Material.AIR.getId(), -1, Material.AIR.getId()},
+        		},
+        		{
+        			{Material.AIR.getId(), -1, Material.AIR.getId()},
+        		},
+        		{
+        			{Material.AIR.getId(), -1, Material.AIR.getId()},
+        		}
+        }));
+    }
+    
+    @Override
+    public boolean onRuneRightClick(BlockRightClickEvent event) {
+        Block block = event.getBlock();
+        
+        // look for a door
+    	Door door = getDoor(block.getLocation());
+    	
+    	if(door == null) {
+    		// no door, is the rune valid?
+            if (tryRune(block)) {
+
+        		// time to create a door!
+        		door = new Door();
+        		door.key = block.getFace(BlockFace.DOWN, 1).getTypeId();
+        		if(door.key <= 0) {
+        			// invalidate the key for air
+        			door.key = -1;
+        		}
+        		door.loc = block.getLocation();
+        		// SAVE!
+        		saveDoor(door);
+        		
+        		// now we reset the door
+        		block.getFace(BlockFace.DOWN, 1).setType(block.getType());
+        		block.getFace(BlockFace.DOWN, 2).setType(block.getType());
+        		
+        		event.getPlayer().sendMessage("Door accepted, fill in the side holes to conceal");	
+            		
+                return true;
+            }
+    	} else {
+    		if(block.getFace(BlockFace.DOWN, 1).getTypeId() == Material.AIR.getId()) {
+    			// door is showing, hide that bitch!
+        		block.getFace(BlockFace.DOWN, 1).setType(block.getType());
+        		block.getFace(BlockFace.DOWN, 2).setType(block.getType());
+    		} else {
+    			// door is hiding, show that bitch!
+    			if(door.key >= 0) {
+    				// we have a key, make sure the key is present
+    				boolean keyFound = false;
+    				Block curBlock;
+    				if((curBlock = block.getFace(BlockFace.DOWN, 2).getFace(BlockFace.NORTH)).getTypeId() == door.key) {
+    					curBlock.setType(Material.AIR);
+    					keyFound = true;
+    				} else if((curBlock = block.getFace(BlockFace.DOWN, 2).getFace(BlockFace.EAST)).getTypeId() == door.key) {
+    					curBlock.setType(Material.AIR);
+    					keyFound = true;
+    				} else if((curBlock = block.getFace(BlockFace.DOWN, 2).getFace(BlockFace.SOUTH)).getTypeId() == door.key) {
+    					curBlock.setType(Material.AIR);
+    					keyFound = true;
+    				} else if((curBlock = block.getFace(BlockFace.DOWN, 2).getFace(BlockFace.WEST)).getTypeId() == door.key) {
+    					curBlock.setType(Material.AIR);
+    					keyFound = true;
+    				}
+    				
+    				if(!keyFound) {
+    					return false;
+    				}
+    			}
+    			
+    			block.getFace(BlockFace.DOWN, 1).setType(Material.AIR);
+        		block.getFace(BlockFace.DOWN, 2).setType(Material.AIR);
+    		}
+    		
+    		return true;
+    	}
+        
+        return false;
+    }
+    
+    @Override
+    public boolean onRuneDamage(BlockDamageEvent event) {
+    	if(event.getDamageLevel() == BlockDamageLevel.BROKEN) {
+    		Block block = event.getBlock();
+    		Door door = null;
+    		
+    		for(int i = 0; i < 3 && door == null; i++) {
+    			block = event.getBlock().getFace(BlockFace.UP, i);
+    			door = getDoor(block.getLocation());
+    		}
+    		
+    		if(door != null) {
+    			deleteDoor(door);
+    			event.getPlayer().sendMessage("Door rune destroyed");
+    			
+    			// players don't get minerals back for this 
+    			event.setCancelled(true);
+    			
+    			// wipe out the door
+    			block.setType(Material.AIR);
+    			block.getFace(BlockFace.DOWN, 1).setType(Material.AIR);
+    			block.getFace(BlockFace.DOWN, 2).setType(Material.AIR);
+    			return true;
+    		}
+    	}
+    	
+    	return false;
+    }
+    
+    private class Door {
+    	public Location loc;
+    	public int key;
+    }
+    
+    private Door getDoor(Location loc) {
+    	Connection sqlConn = null;
+    	Door d = null;
+        File dbfile = new File(mPlugin.getDataFolder(), NAME + ".db");
+        try {
+        	sqlConn = DriverManager.getConnection("jdbc:sqlite:" + dbfile.getAbsolutePath());
+        	
+        	PreparedStatement stmt = sqlConn.prepareStatement(
+        			"select * from doors where x = ? and y = ? and z = ?");
+        	stmt.setInt(1, loc.getBlockX());
+        	stmt.setInt(2, loc.getBlockY());
+        	stmt.setInt(3, loc.getBlockZ());
+        	
+        	ResultSet rs = stmt.executeQuery();
+        	
+        	if(rs.next()) {
+        		d = new Door();
+        		d.loc = loc;
+        		d.key = rs.getInt("key");
+        	}
+        	rs.close();
+        } catch(Exception e) {
+        	d = null;
+        } finally {
+        	if(sqlConn != null) {
+        		try {
+        			sqlConn.close();
+        		} catch(Exception e) { }
+        	}
+        }
+        
+        return d;
+    }
+    
+    private void saveDoor(Door d) {
+    	Connection sqlConn = null;
+        File dbfile = new File(mPlugin.getDataFolder(), NAME + ".db");
+        try {
+        	sqlConn = DriverManager.getConnection("jdbc:sqlite:" + dbfile.getAbsolutePath());
+        	
+        	Statement stmt = sqlConn.createStatement();
+        	stmt.executeUpdate("create table if not exists doors (x, y, z, key);");
+        	
+        	PreparedStatement pstmt = sqlConn.prepareStatement(
+        			"insert into doors values (?, ?, ?, ?);");
+        	pstmt.setInt(1, d.loc.getBlockX());
+        	pstmt.setInt(2, d.loc.getBlockY());
+        	pstmt.setInt(3, d.loc.getBlockZ());
+        	pstmt.setInt(4, d.key);
+        	pstmt.executeUpdate();
+        } catch(Exception e) { 
+        } finally {
+        	if(sqlConn != null) {
+        		try {
+        			sqlConn.close();
+        		} catch(Exception e) { }
+        	}
+        }
+    }
+    
+    private void deleteDoor(Door d) {
+    	Connection sqlConn = null;
+        File dbfile = new File(mPlugin.getDataFolder(), NAME + ".db");
+        try {
+        	sqlConn = DriverManager.getConnection("jdbc:sqlite:" + dbfile.getAbsolutePath());
+        	
+        	PreparedStatement pstmt = sqlConn.prepareStatement(
+        			"delete from doors where x = ? and y = ? and z = ?");
+        	pstmt.setInt(1, d.loc.getBlockX());
+        	pstmt.setInt(2, d.loc.getBlockY());
+        	pstmt.setInt(3, d.loc.getBlockZ());
+        	pstmt.executeUpdate();
+        } catch(Exception e) { 
+        } finally {
+        	if(sqlConn != null) {
+        		try {
+        			sqlConn.close();
+        		} catch(Exception e) { }
+        	}
+        }
+    }
+}
