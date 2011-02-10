924f7e8c51244335a5e0b7777b977a73f3a1befb
cvpcs <root@cvpcs.org>
2011-02-09 22:26:05 -0600

    tier comparison fucking reversed????? OMG NO

--- org/cvpcs/bukkit/magickraft/runestruct/RNTier.java
+++ org/cvpcs/bukkit/magickraft/runestruct/RNTier.java
@@ -35,7 +35,7 @@ public class RNTier implements IRuneNode {
 
 	@Override
 	public boolean isValid(Block b) {
-		return (TierUtility.getTier(b) >= mTier);
+		return (mTier >= TierUtility.getTier(b));
 	}
 
 	@Override
