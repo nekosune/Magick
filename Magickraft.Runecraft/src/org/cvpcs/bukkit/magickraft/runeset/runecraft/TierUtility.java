01c19f8f51cb96e8c7b00daa31971009f15e66bc
cvpcs <root@cvpcs.org>
2011-02-16 18:19:53 -0600

    updated tiers

--- Magickraft.Runecraft/src/org/cvpcs/bukkit/magickraft/runeset/runecraft/TierUtility.java
+++ Magickraft.Runecraft/src/org/cvpcs/bukkit/magickraft/runeset/runecraft/TierUtility.java
@@ -13,6 +13,9 @@ public class TierUtility {
         TIERS.put(Material.DIRT.getId(), 0);
         TIERS.put(Material.GRASS.getId(), 0);
         TIERS.put(Material.STONE.getId(), 0);
+        TIERS.put(Material.SNOW.getId(), 0);
+        TIERS.put(Material.SIGN.getId(), 0);
+        TIERS.put(Material.SIGN_POST.getId(), 0);
         // tier 1
         TIERS.put(Material.COBBLESTONE.getId(), 1);
         TIERS.put(Material.YELLOW_FLOWER.getId(), 1);
@@ -26,6 +29,11 @@ public class TierUtility {
         TIERS.put(Material.SOUL_SAND.getId(), 1);
         TIERS.put(Material.WOOL.getId(), 1);
         TIERS.put(Material.SANDSTONE.getId(), 1);
+        TIERS.put(Material.MOB_SPAWNER.getId(), 1);
+        TIERS.put(Material.SPONGE.getId(), 1);
+        TIERS.put(Material.SNOW_BLOCK.getId(), 1);
+        TIERS.put(Material.ICE.getId(), 1);
+        TIERS.put(Material.SAPLING.getId(), 1);
         // tier 2
         TIERS.put(Material.CHEST.getId(), 2);
         TIERS.put(Material.SOIL.getId(), 2);
@@ -38,18 +46,25 @@ public class TierUtility {
         TIERS.put(Material.BROWN_MUSHROOM.getId(), 2);
         TIERS.put(Material.STEP.getId(), 2);
         TIERS.put(Material.DOUBLE_STEP.getId(), 2);
-        TIERS.put(Material.COBBLESTONE_STAIRS.getId(), 2);
         TIERS.put(Material.WOOD_STAIRS.getId(), 2);
+        TIERS.put(Material.COBBLESTONE_STAIRS.getId(), 2);
         TIERS.put(Material.WATER.getId(), 2);
         TIERS.put(Material.STATIONARY_WATER.getId(), 2);
         TIERS.put(Material.WORKBENCH.getId(), 2);
         TIERS.put(Material.GLOWSTONE.getId(), 2);
         TIERS.put(Material.NOTE_BLOCK.getId(), 2);
+        TIERS.put(Material.LEVER.getId(), 2);
+        TIERS.put(Material.FENCE.getId(), 2);
+        TIERS.put(Material.PUMPKIN.getId(), 2);
+        TIERS.put(Material.STONE_PLATE.getId(), 2);
+        TIERS.put(Material.WOOD_PLATE.getId(), 2);
+        TIERS.put(Material.CROPS.getId(), 2);
         // tier 3
         TIERS.put(Material.BRICK.getId(), 3);
         TIERS.put(Material.CACTUS.getId(), 3);
         TIERS.put(Material.CLAY.getId(), 3);
         TIERS.put(Material.IRON_ORE.getId(), 3);
+        TIERS.put(Material.COAL_ORE.getId(), 3);
         TIERS.put(Material.REDSTONE_ORE.getId(), 3);
         TIERS.put(Material.SUGAR_CANE_BLOCK.getId(), 3);
         TIERS.put(Material.TORCH.getId(), 3);
@@ -57,6 +72,8 @@ public class TierUtility {
         TIERS.put(Material.LAPIS_ORE.getId(), 3);
         TIERS.put(Material.CAKE_BLOCK.getId(), 3);
         TIERS.put(Material.REDSTONE_WIRE.getId(), 3);
+        TIERS.put(Material.JACK_O_LANTERN.getId(), 3);
+        TIERS.put(Material.RAILS.getId(), 3);
         // tier 4
         TIERS.put(Material.BEDROCK.getId(), 4);
         TIERS.put(Material.BOOKSHELF.getId(), 4);
@@ -68,6 +85,8 @@ public class TierUtility {
         TIERS.put(Material.GOLD_BLOCK.getId(), 5);
         TIERS.put(Material.JUKEBOX.getId(), 5);
         TIERS.put(Material.OBSIDIAN.getId(), 5);
+        TIERS.put(Material.DIAMOND_ORE.getId(), 5);
+        TIERS.put(Material.PORTAL.getId(), 5);
         // tier 6
         TIERS.put(Material.DIAMOND_BLOCK.getId(), 6);
     }
