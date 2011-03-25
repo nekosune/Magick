3f79332f80a64e5b652d8c99a15db068591fd1ba
Katrina Swales <kat.swales@nekokittygames.com>
2011-03-25 18:41:02 +0000

    ltered to fit 568, and altered to own namespace, also fixed minor bugs with magic beacon

--- Magickraft/src/org/cvpcs/bukkit/magickraft/runestruct/IRuneNode.java
+++
@@ -1,9 +0,0 @@
-package org.cvpcs.bukkit.magickraft.runestruct;
-
-import org.bukkit.block.Block;
-
-public interface IRuneNode {
-
-    boolean isValid(Block b);
-
-}
--- Magickraft/src/org/cvpcs/bukkit/magickraft/runestruct/IRuneNodeDataParser.java
+++
@@ -1,9 +0,0 @@
-package org.cvpcs.bukkit.magickraft.runestruct;
-
-public interface IRuneNodeDataParser {
-
-    String getNodeTypeId();
-
-    IRuneNode parseNodeData(RuneNodeParser parser, String txt);
-
-}
